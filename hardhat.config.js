61641ec153e89114bd1f19c027233af9da629d81
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-10-23 23:02:58 -0400

    hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+const { ALCHEMY_API_KEY, MUMBAI_PRIVATE_KEY } = process.env;
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: { 
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [MUMBAI_PRIVATE_KEY]
+    }
+  }
 };
+
+
+
